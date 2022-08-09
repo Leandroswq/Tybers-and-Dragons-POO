aa25c91a0815b3b70bb7c4a87534706c444f401d
Leandroswq <leandrobonfim15@hotmail.com>
2022-08-09 19:55:02 -0300

    feat: Cria o type GeneralFighter

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,8 +1,11 @@
 import Fighter from './Fighter';
 import SimpleFighter from './SimpleFighter';
 
+type GeneralFighter = Fighter | SimpleFighter;
+
 export default Fighter;
 
 export {
   SimpleFighter,
+  GeneralFighter,
 };
