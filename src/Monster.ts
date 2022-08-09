3730685b65d9dc46ce8706d70e0c779e7baf4faa
Leandroswq <leandrobonfim15@hotmail.com>
2022-08-09 19:56:06 -0300

    feat: Cria a Classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import { GeneralFighter, SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number { return this._lifePoints; }
+
+  get strength(): number { return this._strength; }
+
+  attack(enemy: GeneralFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints; 
+    if (damage > 0) {
+      if (this.lifePoints <= damage) {
+        this._lifePoints = -1;
+      } else {
+        this._lifePoints -= damage;
+      }
+    }
+    return this.lifePoints;
+  }
+}
